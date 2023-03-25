223954d0d8f65a5de42fbf57329517b59a97138c
Jagger85 <jagger85.crypto@gmail.com>
2023-03-25 16:11:03 +0800

    FEAT:Added Pokemon object, now PokeApi renders a pokemon list on the screen, also downloaded a gif for loading time but not implemented

--- hw-react-pokedex/src/components/PokeApi.js
+++ hw-react-pokedex/src/components/PokeApi.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Pokemon from "./Pokemon";
 
 export default class PokeApi extends Component {
   state = {
@@ -12,14 +13,28 @@ export default class PokeApi extends Component {
     try {
       fetch("https://pokeapi.co/api/v2/pokemon")
         .then((res) => res.json())
-        .then((data) => this.setState({ pokeList: data }))
-        .then(this.setState({ isLoading: false }));
+        .then((data) => {
+          this.setState({ pokeList: data });
+          this.setState({ isLoading: false });
+          console.log(this.state.isLoading);
+          console.log(this.state.pokeList);
+        });
     } catch (error) {
       throw error;
     }
   }
   render() {
-    !this.state.isLoading && console.log(this.state.pokeList);
-    return <div>PokeApi</div>;
+    return (
+      <ol>
+      
+        {this.state.isLoading ? (
+          <div>Is loading...</div>
+        ) : (
+          this.state.pokeList.results.map(( e, i) => {
+            return <Pokemon key={i} name={e.name} url={e.url} />
+          })
+        )}
+      </ol>
+    );
   }
 }
