b2e717d74663010508a8839785089d6876e7cbae
Jagger85 <jagger85.crypto@gmail.com>
2023-03-25 15:16:22 +0800

    FEAT: Added index style & website title

--- hw-react-pokedex/src/index.js
+++ hw-react-pokedex/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/index.css';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
- 
